42dc0fa595f6b2dc45d50f6a21ec02bc2ca4074e
Vlad Bogodyazh <vladbg53@gmail.com>
2022-02-20 12:22:11 +0300

    Тернарный оператор

--- js/main.js
+++ js/main.js
@@ -1,20 +1,12 @@
 function getRandomInt(min, max) {
-  if (min < 0 || max < 0) {
-    return 'Вводите только положительные числа';
-  }
-
-  return Math.floor(Math.random() * (max - min) + min);
+  return (min < 0 || max < 0) ? 'Вводите только положительные числа' : Math.floor(Math.random() * (max - min) + min);
 }
 
 getRandomInt();
 
 
 function getRandomFloat(min, max, length) {
-  if (min < 0 || max < 0) {
-    return 'Вводите только положительные числа';
-  }
-
-  return (Math.random() * (max - min) + min).toFixed(length);
+  return (min < 0 || max < 0) ? 'Вводите только положительные числа' : (Math.random() * (max - min) + min).toFixed(Math.floor(length));
 }
 
 getRandomFloat();
