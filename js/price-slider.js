758aa17b116a3a79855db53445156b4f03a8fe2e
Vlad Bogodyazh <vladbg53@gmail.com>
2022-03-23 20:40:51 +0300

    закончен слайдер

--- js/price-slider.js
+++ js/price-slider.js
@@ -6,11 +6,15 @@ noUiSlider.create(sliderPrice, {
     min: 0,
     max: 100000,
   },
-  start: 5000,
+  start: 1000,
   step: 100,
   connect: 'lower',
 });
 
 sliderPrice.noUiSlider.on('update', () => {
   inputPrice.value = sliderPrice.noUiSlider.get();
+});
+
+inputPrice.addEventListener('change', () => {
+  sliderPrice.noUiSlider.set(inputPrice.value);
 });
